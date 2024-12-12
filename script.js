d664cd4cf0a2b2a12b784d979ac8a20cf52a6a30
Darron Dai <daidarron@gmail.com>
2024-12-12 12:57:07 -0800

    Add wincheck logic and markcell edgecase handling

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // gameboard is obj with a 3x3 array, some state of the game aka markings, needs a way to mark a cell
 // multiple gameboards may be made (make a new game)
-const createGameboard = function (rowCount, colCount) {
+const createGameboard = function () {
   // row and col count are both 3
   const ROW_COUNT = 3;
   const COL_COUNT = 3;
@@ -31,9 +31,20 @@ const createGameboard = function (rowCount, colCount) {
     }
   };
   // function to mark a cell given an i, j, and a mark
-  // MAY NEED LOGIC TO AVOID OVERWRITING
+  // MAY NEED LOGIC TO AVOID OVERWRITING / out of bounds
   const markCell = (x, y, mark) => {
+    // handle out of bounds
+    if (x < 0 || y < 0 || x >= ROW_COUNT || y >= COL_COUNT) {
+      console.log("Pick a cell within 3x3 bounds!");
+      return false;
+    }
+    // avoid overwriting
+    if (board_state[x][y] != "") {
+      console.log(`Cell ${x},${y} already marked! Pick a different cell`);
+      return false;
+    }
     board_state[x][y] = mark;
+    return true;
   };
 
   return { getBoard, printBoard, markCell };
@@ -62,25 +73,102 @@ const gameController = (function (
   player2Name = "player2"
 ) {
   // create gameboard and player instances
-  let board = createGameboard(3, 3);
+  let board = createGameboard();
   const player1 = createPlayer(player1Name, "X");
   const player2 = createPlayer(player2Name, "O");
-
+  // state variable for finished game
   // state variable for active player, along with toggle func
   let activePlayer = player1;
   const getActivePlayer = () => activePlayer;
-  const toggleActivePlayer = () =>
-    activePlayer === player1 ? player2 : player1;
+  const toggleActivePlayer = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+  };
   // func for check win
-  const checkWin = function () {};
+  const checkWin = function () {
+    // check rows, cols and diagonals
+    let boardState = board.getBoard();
+
+    const activeMark = activePlayer.getMark();
+    // rows
+    if (
+      boardState[0][0] == activeMark &&
+      boardState[0][1] == activeMark &&
+      boardState[0][2] == activeMark
+    ) {
+      return true;
+    }
+    if (
+      boardState[1][0] == activeMark &&
+      boardState[1][1] == activeMark &&
+      boardState[1][2] == activeMark
+    ) {
+      return true;
+    }
+    if (
+      boardState[2][0] == activeMark &&
+      boardState[2][1] == activeMark &&
+      boardState[2][2] == activeMark
+    ) {
+      return true;
+    }
+    // cols
+    if (
+      boardState[0][0] == activeMark &&
+      boardState[1][0] == activeMark &&
+      boardState[2][0] == activeMark
+    ) {
+      return true;
+    }
+    if (
+      boardState[0][1] == activeMark &&
+      boardState[1][1] == activeMark &&
+      boardState[2][1] == activeMark
+    ) {
+      return true;
+    }
+    if (
+      boardState[0][2] == activeMark &&
+      boardState[1][2] == activeMark &&
+      boardState[2][2] == activeMark
+    ) {
+      return true;
+    }
+    // diagonals
+    if (
+      boardState[0][0] == activeMark &&
+      boardState[1][1] == activeMark &&
+      boardState[2][2] == activeMark
+    ) {
+      return true;
+    }
+    if (
+      boardState[2][0] == activeMark &&
+      boardState[1][1] == activeMark &&
+      boardState[0][2] == activeMark
+    ) {
+      return true;
+    }
+
+    // no 3 in a row means no win
+    return false;
+  };
   // func for playing a turn (check win at the end of each turn)
-  const playRound = function () {
+  const playRound = function (row, col) {
     // round logic
+    // // print the board
+    // board.printBoard();
+    // pick a cell to mark with the current player's mark
+    board.markCell(row, col, activePlayer.getMark());
+    // print board after marking
+    board.printBoard();
     // check win at end of turn
     if (checkWin()) {
       // change state????
-      console.log("game over");
+      console.log(`Game Over! ${activePlayer.getName()} wins!!!`);
+      return;
     }
+    // swap active player for next turn
+    toggleActivePlayer();
   };
   // func for new game
   const startNewGame = function () {
@@ -93,9 +181,7 @@ const gameController = (function (
 
   return {
     getActivePlayer,
-    toggleActivePlayer,
     playRound,
     startNewGame,
-    printGameboard,
   };
 })();
